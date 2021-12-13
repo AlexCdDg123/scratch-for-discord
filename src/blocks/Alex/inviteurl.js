934d42d40d311b1c321c71811d727c606e4485e2
AlexCdDg <95496915+AlexCdDg123@users.noreply.github.com>
2021-12-12 22:11:25 -0300

    Create inviteurl.js

---
+++ src/blocks/Alex/inviteurl.js
@@ -0,0 +1,21 @@
+import * as Blockly from "blockly/core";
+
+const blockName = "invite_ur";
+
+const blockData = {
+  "message0": "Invite URL",
+  "colour": "#502e6e",
+  "tooltip": "",
+  "helpUrl": ""
+}
+
+Blockly.Blocks[blockName] = {
+  init: function () {
+    this.jsonInit(blockData);
+  }
+};
+
+Blockly.JavaScript[blockName] = function (block) {
+  const code =  `invite.url`;
+  return code;
+};
